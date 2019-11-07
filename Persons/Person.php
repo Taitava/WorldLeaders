c1d19b3a85ec4710b24a0de19daf8e88f103d15b
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2019-11-07 19:06:24 +0200

    Person: create a new reference() method for remarking sources of information.

--- Persons/Person.php
+++ Persons/Person.php
@@ -38,4 +38,16 @@ class Person
 	{
 		return new PhoneCall($this, $to);
 	}
+	
+	/**
+	 * Can be used to express external references to certain sentences said by a given Person.
+	 *
+	 * @param string $index A short indicator like "[1]" to tell ehich part of the text this reference is related to.
+	 * @param string $source Can be a URL pointing to a webpage as the source of information. Can also be for example a book (author name, book name, publisher name, year and page number).
+	 * @param string $date_checked A date when the source was read or last checked.
+	 */
+	public function reference($index, $source, $date_checked)
+	{
+	
+	}
 }
